7c5d3317d2d1521250286c50d7d9c33779825af8
Tomasz Drozd <tpdrozd@gmail.com>
2018-04-23 19:00:47 +0200

    Enkapsulacja Markera i InfoWindow (z Google map) do klasy InfoMarker.

--- web/ng/mapModule.js
+++ web/ng/mapModule.js
@@ -5,12 +5,83 @@ angular.module('mapModule', [])
 
 function mapCtrl($scope) {
 	
-	$scope.$on('LocalitySelected',
-		function(event, locality) {
-			var centralPoint = new google.maps.LatLng(locality.lat, locality.lon);
-			$scope.map.setCenter(centralPoint);
-			
+	$scope.gmap = null;
+	infoMarker = null;
+	
+	$scope.$on('LocalitySelected', function(event, locality) {
+		showLocality(locality);
 	});
+	
+	function showLocality (locality) {
+		// ustawienie markera
+		if (infoMarker == null) {
+			infoMarker = new InfoMarker($scope.gmap);
+			$scope.gmap.setZoom(10);
+		}
+		infoMarker.pointTo(locality);
+
+		// wycentrowanie mapy
+		$scope.gmap.setCenter(infoMarker.position);
+	}
+	
+	function InfoMarker(map) {
+		var gmap = map;
+		var marker = null;
+		var infoWn = null;
+		var opened = false;
+		
+		this.position = null;
+		this.pointTo = function(locality) {
+			this.position = new google.maps.LatLng(locality.lat, locality.lon);
+			marker.setPosition(this.position);
+			marker.setTitle(locality.name);
+			infoWn.setContent(infoContent(locality));
+		}
+		
+		init();
+		
+		function init() {
+			// inicjalizacja markera
+			marker = new google.maps.Marker({map: gmap});
+			google.maps.event.addListener(marker, 'click', function() {
+				if (opened) {
+					infoWn.close();
+					opened = false;
+				}
+				else {
+					infoWn.open(gmap, marker);
+					opened = true;
+				}
+			});
+			
+			// inicjalizacja infoWindow
+			infoWn = new google.maps.InfoWindow();
+			google.maps.event.addListener(infoWn, 'closeclick', function() {
+				opened = false;
+			});
+		} // end of init
+		
+		function infoContent(loc) {
+			// pierwszy wiersz
+			var content = "<b>" + loc.name + "</b>";
+			if (loc.collateralName)
+				content = content + " / <b><i>" + loc.collateralName + "</i></b>";
+			if (loc.historicalName)
+				content = content + " / <b><i>" + loc.historicalName + "</i></b>";
+			if (loc.foreignName)
+				content = content + " / <b><i>" + loc.foreignName + "</i></b>";
+			if (loc.foreignLatin)
+				content = content + " (<b><i>" + loc.foreignLatin + "</i></b>)";
+			
+			// drugi wiersz
+			content = content + "<br/>";
+			content = content + "<i>" + loc.type + "</i>";
+			if (loc.parentName)
+				content = content + " " + loc.parentName;
+			
+			return content;
+		}
+	} // end of InfoMarker
 }
 
 function gmap() {
@@ -20,8 +91,7 @@ function gmap() {
 		replace: true,
 		scope: false,
 		link: function(scope, element, attrs) {
-			scope.map = initGmap(element, attrs);
-			
+			scope.gmap = initGmap(element, attrs);
 		}/*end of link*/
 	}
 	
